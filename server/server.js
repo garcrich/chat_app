3d2df98f193d6a10a85cd705f1196e13265c605e
Ricky Garcia <RickyG@Olympus.rsiinc.com>
2017-05-30 12:22:42 -0600

    Greet new user, and aert others

--- server/server.js
+++ server/server.js
@@ -14,14 +14,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit from Admin text Welcome to the chat app
+    //socket.broadcast.emit from Admin text Nes user joined
+socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+});
+
+socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+})
+
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            craetedAt: new Date().getTime()
-        });
+        //io.emit('newMessage', {
+            // from: message.from,
+            // text: message.text,
+            // craetedAt: new Date().getTime()
+            socket.broadcast.emit('newMessage', {
+                from: message.from,
+                text: message.text,
+                createdAt: new Date().getTime()
+            });
     });
 
     socket.on('disconnect', () => {
