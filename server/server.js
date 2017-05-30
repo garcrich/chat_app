922354b747fe600e5782481c1d7efcb31bf03214
Ricky Garcia <RickyG@Olympus.rsiinc.com>
2017-05-30 10:17:35 -0600

    fixed port issue that crashed heroku.

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is up on ${port}`);
 })
 
-console.log(__dirname + '/../public');
+
 console.log(publicPath);
