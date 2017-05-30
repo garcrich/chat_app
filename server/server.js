6d2223dae7782091271f6e5adf288fd0dafb29be
Ricky Garcia <RickyG@Olympus.rsiinc.com>
2017-05-30 15:04:28 -0600

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -19,13 +19,14 @@ io.on('connection', (socket) => {
     //socket.broadcast.emit from Admin text Nes user joined
 socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined.'))
-
-
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback('This is from the server');
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
