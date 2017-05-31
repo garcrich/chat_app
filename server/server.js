fa4b2d9a2b61c48c6c18add6fe592276e7fb01ce
Ricky Garcia <RickyG@Olympus.rsiinc.com>
2017-05-31 12:44:58 -0600

    Add styling and UX for chat page

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
